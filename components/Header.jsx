5a82e149df00d02bffb8f69d5e2627360d091b74
Samuel Babalola <98273940+Samuelmusic1@users.noreply.github.com>
2026-04-01 08:35:05 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header() {
     return (
-        <img src="images/profileimg.jpeg" alt="" className="dev-img" />
+        <img src="/images/profileimg.jpeg" alt="" className="dev-img" />
     )
 }
