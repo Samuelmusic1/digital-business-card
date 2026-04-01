d55e18d09d45521cc92c828f6d415e0c1529352a
Samuel Babalola <98273940+Samuelmusic1@users.noreply.github.com>
2026-04-01 08:34:48 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,22 +4,22 @@ export default function Footer() {
             <footer>
                 <div>
                     <a>
-                        <img src="./images/twitter.png" alt="twitter logo" className="twt-img"/>
+                        <img src="/images/twitter.png" alt="twitter logo" className="twt-img"/>
                     </a>
                 </div>
                 <div>
                     <a href="">
-                        <img src="./images/facebook.png" alt="facebook logo" className="fb-img"/>
+                        <img src="/images/facebook.png" alt="facebook logo" className="fb-img"/>
                     </a>
                 </div>
                 <div>
                     <a href="">
-                        <img src="./images/instagram.png" alt="instagram logo" className="ig-img"/>
+                        <img src="/images/instagram.png" alt="instagram logo" className="ig-img"/>
                     </a>
                 </div>
                 <div>
                     <a href="https://github.com/Samuelmusic1">
-                        <img src="./images/github.png" alt="github logo" className="git-img"/>
+                        <img src="/images/github.png" alt="github logo" className="git-img"/>
                     </a>
                 </div>
             </footer>
